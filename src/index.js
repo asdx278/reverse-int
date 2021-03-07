c7a289a38f708a68b91ea6189134695dd2850ff6
asdx278 <albert.shubin@gmail.com>
2021-03-07 17:17:42 +0400

    feat: update index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    if(n<0) {
+        n = n*-1;
+        let str = String(n);
+        str = str.split("").reverse().join("");  
+        let num = Number(str);
+        return num;
+    } else {
+        let str = String(n);
+        str = str.split("").reverse().join("");  
+        let num = Number(str);
+        return num;
+    }
 }
